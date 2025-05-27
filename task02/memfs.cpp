c66b59aa4490b43f0734744d6708a82c68feb815
Aaron Leontaris <aaron.leontaris@web.de>
2025-05-27 13:45:53 +0200

    sadsa

--- task02/memfs.cpp
+++ task02/memfs.cpp
@@ -4,7 +4,13 @@
 #include <sys/stat.h>
 #include <unistd.h>
 #include <pwd.h>
+#if __has_include(<json/json.h>)
 #include <json/json.h>
+#elif __has_include(<jsoncpp/json/json.h>)
+#include <jsoncpp/json/json.h>
+#else
+#error "jsoncpp headers not found"
+#endif
 
 MemFS memfs;
 
